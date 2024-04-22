f2480175e77121098dc1415cbd915ac14b4d58ed
alhoss99 <alhoss99@gmail.com>
2024-04-22 04:23:09 +0300

    git push

---
+++ Linear Algebra for Machine Learning and Data Science/gbh/WorldOfTechnology/CollegeCourses.php
@@ -0,0 +1,85 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="design.css">
+</head>
+
+<body>
+    <nav class="section__container nav__container">
+        <div class="nav__logo">
+            <img src="img1.png" alt="logo" />
+            <span>World Of Technology</span>
+        </div>
+        <ul class="nav__links">
+            <li class="link"><a href="index.php">College Specializations</a></li>
+            <li class="link"><a href="CollegeEvents.php">College Events</a></li>
+            <li class="link"><a href="CollegeCourses.php">College Courses</a></li>
+            <li class="link"><a href="login.php">logout</a></li>
+        </ul>
+    </nav>
+    <section id="blog">
+        <div class="blog-heading">
+            <h3>College Events</h3>
+        </div>
+
+
+        <div class="blog-container">
+
+            <div class="blog-box">
+
+                <div class="blog-img">
+                    <img alt="blog" src="post7.jpg">
+                </div>
+                <div class="blog-text">
+                    <a href="#" class="blog-title">E-Marketing</a>
+                    <p>E-marketing is considered one of the general branches of marketing, and it is also called
+                        (digital marketing) or (network marketing), and it is the strategy that is used in organizing
+                        methods of modern communications technology.</p>
+                    <a href="#">Read More</a>
+                </div>
+
+            </div>
+
+            <div class="blog-box">
+
+                <div class="blog-img">
+                    <img alt="blog" src="post8.webp">
+                </div>
+                <div class="blog-text">
+                    <a href="#" class="blog-title">Creativity techniques</a>
+                    <p>
+                        Creativity techniques are designed to increase the amount of new ideas. This approach is based
+                        on
+                        the belief that the greater the number of ideas, the greater the chance that one of them will
+                        have value. Some of these techniques include randomly selecting an idea (such as choosing a word
+                        from a list), thinking of analogies to the undesirable situation, and encouraging related
+                        inspirational thoughts that lead to a solution. These techniques are described in Creativity
+                        Techniques.
+                    </p>
+                    <a href="#">Read More</a>
+
+                </div>
+
+            </div>
+
+            <div class="blog-box">
+
+                <div class="blog-img">
+                    <img alt="blog" src="post9.webp">
+                </div>
+                <div class="blog-text">
+                    <a href="#" class="blog-title">HR</a>
+                    <p>As its name indicates, the specialty of human resources management is the administrative field
+                        specialized in everything related to human resources in the work environment</p>
+                    <a href="#">Read More</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+</body>
+
+</html>
---
+++ Linear Algebra for Machine Learning and Data Science/gbh/WorldOfTechnology/CollegeEvents.php
@@ -0,0 +1,85 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="design.css">
+</head>
+
+<body>
+    <nav class="section__container nav__container">
+        <div class="nav__logo">
+            <img src="img1.png" alt="logo" />
+            <span>World Of Technology</span>
+        </div>
+        <ul class="nav__links">
+            <li class="link"><a href="index.php">College Specializations</a></li>
+            <li class="link"><a href="CollegeEvents.php">College Events</a></li>
+            <li class="link"><a href="CollegeCourses.php">College Courses</a></li>
+            <li class="link"><a href="login.php">logout</a></li>
+        </ul>
+    </nav>
+    <section id="blog">
+        <div class="blog-heading">
+            <h3>College Events</h3>
+        </div>
+
+
+        <div class="blog-container">
+
+            <div class="blog-box">
+
+                <div class="blog-img">
+                    <img alt="blog" src="post4.jpg">
+                </div>
+                <div class="blog-text">
+                    <a href="#" class="blog-title">Effectiveness of blood donation</a>
+                    <p>The slogan of the World Blood Donor Day 2023 campaign is “Donate blood, donate plasma, share
+                        life, share a lot.”.</p>
+                    <a href="#">Read More</a>
+                </div>
+
+            </div>
+
+
+
+
+            <div class="blog-box">
+
+                <div class="blog-img">
+                    <img alt="blog" src="post5.jpg">
+                </div>
+                <div class="blog-text">
+                    <a href="#" class="blog-title">Running race activity</a>
+                    <p>The idea of ​​the annual charity running race emerged from the core of the goodness inherent in
+                        the souls of the people of the Eastern Province and their love for their country and their
+                        community. This pioneering idea was promoted by a number of loyal businessmen and
+                        philanthropists, in addition to the parents of a number of students.</p>
+                    <a href="#">Read More</a>
+                </div>
+
+            </div>
+
+
+
+
+            <div class="blog-box">
+
+                <div class="blog-img">
+                    <img alt="blog" src="post6.webp">
+                </div>
+                <div class="blog-text">
+                    <a href="#" class="blog-title">Photography effectiveness</a>
+                    <p>The Photography Exhibition is the most prominent international event in the region in the
+                        photography, video, and photo printing sectors. The exhibition is considered the ideal place to
+                        learn the opinions of experts and learn the latest news from the photography community.</p>
+                    <a href="#">Read More</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+</body>
+
+</html>
